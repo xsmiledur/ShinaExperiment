acbf420c15212176c4aae53c26717fcdd87434e1
Yuri Mikawa <xsmiledur.8x7@gmail.com>
2017-06-12 16:48:50 +0900

    SQLインジェクション

--- html/search.php
+++ html/search.php
@@ -9,7 +9,9 @@
  * ②SQL呼び出しに伴う脆弱性（安全なWebアプリケーションの作り方 p123~）
  *
  * 脆弱なサイト（SQLインジェクション攻撃によるデータ改ざん）
- *
+ * '; update book_data set title='cracked!
+ * '; update+book_data+set+title%3D'<i>cracked!</i>（URL攻撃の場合こっち）
+ * などと打つと全ての蔵書の著者がcracked!になったり
  *
  * Created by IntelliJ IDEA.
  * User: xsmiledur
@@ -20,53 +22,72 @@
 
 $keyword = $_GET['keyword'];
 
-if ($keyword) {
-    try {
-        // データベースに接続
-        $pdo = new PDO(
-            'mysql:host=mydb;port=3306;charset=utf8;dbname=shina_exp;',
-            'root',
-            'password',
-            [
-                //PDO::MYSQL_ATTR_READ_DEFAULT_FILE => '/etc/mysql/my.cnf',
-                //PDO::MYSQL_ATTR_READ_DEFAULT_GROUP => 'client',
-                PDO::ATTR_EMULATE_PREPARES => false
-            ]
-        );
-        /*
-        $sql = "SELECT * FROM user_data WHERE (email = '" . $email . "' AND pass = '".$pass."');";
-    
-        $data = $pdo->query($sql);
-        var_dump($data);
-        echo "<br><br>";
-        */
+try {
+    // データベースに接続
+    $pdo = new PDO(
+        'mysql:host=mydb;port=3306;charset=utf8;dbname=shina_exp;',
+        'root',
+        'password',
+        [
+            //PDO::MYSQL_ATTR_READ_DEFAULT_FILE => '/etc/mysql/my.cnf',
+            //PDO::MYSQL_ATTR_READ_DEFAULT_GROUP => 'client',
+            //PDO::ATTR_EMULATE_PREPARES => false //ここをfalseにしないとSQLインジェクションが起こる
+        ]
+    );
+    /*
+    $sql = "SELECT * FROM user_data WHERE (email = '" . $email . "' AND pass = '".$pass."');";
+    $data = $pdo->query($sql);
+    var_dump($data);
+    echo "<br><br>";
+    */
 
-        $sql = "SELECT * FROM book_data WHERE author = '" . $keyword . "';";
-        $stmt = $pdo->prepare($sql);
-        $result = $stmt->execute();
-        $data = $stmt->fetch();
-
-
-    } catch (PDOException $e) {
-
-        die($e->getMessage());
-
+    if ($keyword) {
+        $sql = "SELECT * FROM book_data WHERE author = '" . $keyword . "' ORDER BY id;";
+    } else {
+        $sql = "SELECT * FROM book_data ORDER BY id;";
     }
+    $stmt = $pdo->query($sql);
+    $res = $stmt->execute();
+    $data = $stmt->fetchAll();
+    //$data = $stmt->fetchAll();
+/*
+    var_dump($sql);
+    $stmt = $pdo->prepare($sql);
+    $result = $stmt->execute();
+    $data = $stmt->fetchAll();
+*/
+
+
+} catch (PDOException $e) {
+
+    die($e->getMessage());
 
 }
 
 
+
 ?>
 
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>蔵書検索</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="style.css">
+</head>
 <body>
-検索キーワード：<?php echo $keyword; ?>
+<h1>蔵書検索</h1>
+検索キーワード：<?php echo ($keyword) ? $keyword : "なし"; ?>
 
 <form action="" method="GET">
     著者名で検索できます
     <input name="keyword">
+    <button type="submit">検索</button>
 </form>
 
 
+<?php if ($data ) : ?>
 <table>
     <thead>
     <tr>
@@ -91,5 +112,8 @@ if ($keyword) {
     <?php endforeach; ?>
 
 </table>
+<?php else: ?>
+    お探しの条件では結果はありません。
+<?php endif; ?>
 
 </body>
